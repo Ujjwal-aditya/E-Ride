ac445195c49c7526e2cde983efc373a5706f2638
Ujjwal-aditya <76514938+Ujjwal-aditya@users.noreply.github.com>
2022-05-25 11:09:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+
+    //return <BottomTabNavigator>;
+    //<BottomTabNavigator />;
+     <BottomTabNavigator />;
+    //return <BottomTabNavigator> <BottomTabNavigator />;
+    
+  }
+}
